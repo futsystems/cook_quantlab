03a2633c9899a1a56a76dbb4db16accd7377b515
Vincent <develop@unipayment.io>
2022-06-10 16:46:54 +0800

    script

---
+++ build_datafarm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+supervisorctl stop srv.datafarm
+
+
+dotnet build Srv.DataFarm/src/Application -c release --no-cache --output /opt/srv.datafarm/release
+
+if [ -f "/opt/srv.datafarm/appsettings.Production.json" ]; then
+	cp -f /opt/srv.datafarm/appsettings.Production.json /opt/srv.datafarm/release
+fi
+
+supervisorctl start srv.datafarm
