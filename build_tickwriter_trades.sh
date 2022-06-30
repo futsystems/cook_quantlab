c20d933d02cd622f3554fe213d66c7ffdecf20f0
Vincent <develop@unipayment.io>
2022-06-30 09:18:40 +0800

    script

--- build_tickwriter_trades.sh
+++ build_tickwriter_trades.sh
@@ -2,13 +2,13 @@
 
 git pull
 
-supervisorctl stop srv.tickwriter.trades
+supervisorctl stop srv.tickwriter.trade
 
 
-dotnet build TickWriter/ -c release --no-cache --output /opt/srv.tickwriter.trades/release
+dotnet build TickWriter/ -c release --no-cache --output /opt/srv.tickwriter.trade/release
 
-if [ -f "/opt/srv.tickwriter.trades/appsettings.Production.json" ]; then
-	cp -f /opt/srv.tickwriter.trades/appsettings.Production.json /opt/srv.tickwriter.trades/release
+if [ -f "/opt/srv.tickwriter.trade/appsettings.Production.json" ]; then
+	cp -f /opt/srv.tickwriter.trade/appsettings.Production.json /opt/srv.tickwriter.trade/release
 fi
 
-supervisorctl start srv.tickwriter.trades
+supervisorctl start srv.tickwriter.trade
